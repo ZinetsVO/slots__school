e1cf3639807d9be92dc3967500ceea587dea21a7
ZinetsVO <zinets.vo@gmail.com>
2024-04-03 02:24:40 +0300

    second try popup made

--- src/components/Slot/index.jsx
+++ src/components/Slot/index.jsx
@@ -4,16 +4,16 @@ import css from "./style.module.css";
 import { salts } from "@/app/data";
 import { useProduct } from "@/components/Context";
 import SlotCounter from "react-slot-counter";
-
+import { MdWidthFull } from "react-icons/md";
 
 const Slot = () => {
   const { inventory, spins, toggleSpin, setInventory } = useProduct();
   const [cells, setCells] = useState(["S", "P", "I", "N"]); // Initial texts (can be any values)
   const [buttonWork, setButtonWork] = useState(false);
+  const [secondTryShow, setSecondTryPopup] = useState(false);
+  const [editingIndex, setEditingIndex] = useState(null);
 
   const spinSlots = () => {
-
- 
     setButtonWork(true);
     if (spins >= 1) {
       toggleSpin(spins - 1);
@@ -35,7 +35,7 @@ const Slot = () => {
         (el) => el.first__el == newValues[0] && el.second__el == newValues[2]
       );
 
-      setTimeout(() => setButtonWork(spins <= 0), 2200);
+      setTimeout(() => setButtonWork(spins <= 0), 2800);
 
       if (index >= 0) {
         const wonElement = salts[index];
@@ -44,6 +44,8 @@ const Slot = () => {
           wonElement.second__index == newValues[3]
         ) {
           setInventory((prevInventory) => [...prevInventory, wonElement]);
+        } else {
+          setTimeout(() => setSecondTryPopup(!secondTryShow), 2700);
         }
       }
     } else {
@@ -64,12 +66,105 @@ const Slot = () => {
   };
   const getRandomIndex = () => {
     // Adjust probabilities here
-    const options = [1, 2, 3];
+    const options = ["1", "2", "3"];
     return options[Math.floor(Math.random() * options.length)];
+  };
+
+  const handleEdit = (index, newValue) => {
+    const updatedArray = [...cells];
+    updatedArray[index] = newValue;
+    setCells(updatedArray);
+  };
+
+  const handleFinishEdit = (index, newValue) => {
+    if (newValue) {
+      const updatedArray = [...cells];
+      updatedArray[index] = newValue;
+      setCells(updatedArray);
+      setEditingIndex(null); // Stop editing
+    } else {
+      const updatedArray = [...cells];
+      updatedArray[index] = "0";
+      setCells(updatedArray);
+      setEditingIndex(null);
+    }
+  };
+
+  const handleRemake = () => {
+    const index = salts.findIndex(
+      (el) => el.first__el == cells[0] && el.second__el == cells[2]
+    );
+
+    setSecondTryPopup(false);
+
+    if (
+      salts[index].first__index == cells[1] &&
+      salts[index].second__index == cells[3]
+    ) {
+      alert("Ти повернув свій спін назад :)");
+      toggleSpin(spins + 1);
+    } else {
+      alert("Не правильно :( Вчи хімію краще, щоб наступного разу вдалося");
+    }
   };
 
   return (
     <div className={css.slots__container}>
+      <div
+        className={
+          secondTryShow
+            ? `${css.second__popup} ${css.active}`
+            : `${css.second__popup}`
+        }
+      >
+        <div className={css.popup__inner}>
+          <h2 className={css.second__title}>
+            Програшний спін, але є ще другий шанс!
+          </h2>
+          <p className={css.second__desc}>
+            Застосуй свої знання хімії, щоб зберегти свій спін. Заміни індекси
+            так, щоб формула була правильною
+          </p>
+
+          <div className={css.edit__container}>
+            {cells.map((string, index) => (
+              <div key={index}>
+                {editingIndex === index && index % 2 == 1 ? (
+                  <input
+                    type="text"
+                    value={string}
+                    onChange={(e) => handleEdit(index, e.target.value)}
+                    onBlur={(e) => handleFinishEdit(index, e.target.value)}
+                    className={css.editable__input} // Apply the input field style
+                    style={{ width: `${string.length}ch` }}
+                  />
+                ) : (
+                  <p
+                    onClick={() => setEditingIndex(index)}
+                    className={
+                      index % 2 == 1
+                        ? `${css.editable__string} ${css.pointer}`
+                        : `${css.editable__string}`
+                    } // Apply the <p> tag style
+                  >
+                    {string}
+                  </p>
+                )}
+              </div>
+            ))}
+          </div>
+
+          <button
+            onClick={() => setSecondTryPopup(false)}
+            className={css.close__button}
+          >
+            X
+          </button>
+          <button onClick={handleRemake} className={css.remake__button}>
+            Виправити
+          </button>
+        </div>
+      </div>
       <div className={css.slot__row}>
         {cells.map((text, index) => (
           <div className={css.slot__cell}>
